ac961c907146f7ea72fd4ed36d5e4d625e894d3b
Nagendra386 <nagendrakdv94@gmail.com>
2023-10-24 13:24:33 +0530

    episode-01-react

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ React.createElement("div",{id:"child3"},
 [React.createElement("h1",{id:"heading3-1"},"iam H3-1 tag"),
 React.createElement("h2",{id:"heading3-2"},"iam H3-2 tag")]
 )]);
+
 //console.log(heading);
 const root =ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
